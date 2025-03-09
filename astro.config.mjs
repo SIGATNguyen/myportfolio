c892fdbb7bca3ef1c9d085678f0b95f1c50c9ee4
SIGATNguyen <kevin.lucongsang@gmail.com>
2025-03-09 20:44:45 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+export default defineConfig({
+  // Si tu déploies sur https://SIGATNguyen.github.io/myportfolio, laisse base: '/'
+  // Si tu déploies sur https://SIGATNguyen.github.io/myportfolio/<sous-dossier>/, alors mets base: '/<sous-dossier>/'
+  base: '/myportfolio/',
+
+  // Dossier de sortie pour le build
+  outDir: 'docs', // <-- on génère dans docs au lieu de dist
+
+  // ... tes autres configurations
+});
