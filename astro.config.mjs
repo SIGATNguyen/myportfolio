83d60f0a1b9d8471aa6bd475832bf2848a532b91
SIGATNguyen <kevin.lucongsang@gmail.com>
2025-03-09 20:52:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Si tu déploies sur https://SIGATNguyen.github.io/myportfolio, laisse base: '/'
-  // Si tu déploies sur https://SIGATNguyen.github.io/myportfolio/<sous-dossier>/, alors mets base: '/<sous-dossier>/'
-  base: '/myportfolio/',
-
-  // Dossier de sortie pour le build
-  outDir: 'docs', // <-- on génère dans docs au lieu de dist
-
-  // ... tes autres configurations
+  base: '/', // Pour que les chemins soient corrects sur Vercel
+  outDir: 'docs',
+  // ... autres configurations
 });
