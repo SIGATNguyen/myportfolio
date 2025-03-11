d04854ca4fe6d6afda72ae94b0a35ba772ddd471
SIGATNguyen <kevin.lucongsang@gmail.com>
2025-03-11 21:08:57 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 
 export default defineConfig({
   base: '/', // Pour que les chemins soient corrects sur Vercel
   outDir: 'docs',
-  integrations: [image()],
   // ... autres configurations
 });
